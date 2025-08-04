7e1d3253c6f0890e9c6fe51bf0cbeb42d40d5c83
EnesBelkaya05 <eb.belkaya@gmail.com>
2025-08-04 12:10:24 +0300

    ilk egzersizler

--- exercises.py
+++ exercises.py
@@ -1,3 +1,29 @@
-print("create hammer")
-print("create nails")
-print("use hammer and nails")
+# #first exercise
+# print("create hammer")
+# print("create nails")
+# print("use hammer and nails")
+
+# #second exercise
+# item_name = "headphones"
+# price = 6.99
+# inventory = 371
+# print(item_name, price, inventory)
+#
+# third exercise
+# name = input("enter your name: ")
+# distance = input("enter your distance: ")
+# miles = float(distance)/1.609
+# print(f" Hello {name.title()} !, {distance}km is equals to {miles} miles")
+#
+# fourth  exercise
+# msg="welcome to Python 101: Strings"
+# new_msg=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]
+# print(new_msg.title())
+#
+# fifth exercise
+# name='TERRY'
+# color = 'RED'
+# msg = '[' + name + '] loves the color ' + color.lower() + '!'
+# msg1 = f'[{name.capitalize()}] loves the color {color.lower()}!'
+# print(msg)
+# print(msg1)
